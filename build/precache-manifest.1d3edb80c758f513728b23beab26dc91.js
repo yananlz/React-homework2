1b84c13d0125a8938db52671df05faf787de5ad4
yananlz <15403356239@qq.com>
2020-10-18 13:33:47 +0800

    homework22

---
+++ build/precache-manifest.1d3edb80c758f513728b23beab26dc91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e3c169d3b25a0e25b55da6a5245933",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17553b6d5136502cd0d1",
+    "url": "/static/css/main.6059478d.chunk.css"
+  },
+  {
+    "revision": "fa3e911c185a512b29e0",
+    "url": "/static/js/2.dbda0de3.chunk.js"
+  },
+  {
+    "revision": "de5fa9cfc3624bea3d7565f048bfaaaf",
+    "url": "/static/js/2.dbda0de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17553b6d5136502cd0d1",
+    "url": "/static/js/main.1e20dbe1.chunk.js"
+  },
+  {
+    "revision": "967bfd57e19f8ac017fc",
+    "url": "/static/js/runtime-main.1072f285.js"
+  }
+]);
